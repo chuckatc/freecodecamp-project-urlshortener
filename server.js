b8c0910db714605f336b066bf1801871f9ef9f36
Glitch (humdrum-grape) <none>
2018-11-11 05:02:26 +0000

    🦂🍼 Checkpoint ./server.js:1259819/2

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
 var cors = require('cors');
+
 
 var app = express();
 
