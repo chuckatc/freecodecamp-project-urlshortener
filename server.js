0109ede5cfcb4f53704f7f8bafa0f6a0b5c823eb
Glitch (humdrum-grape) <none>
2018-11-18 22:01:48 +1100

    Tweak ESLint and Prettier

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-'use strict'
-require('dotenv').config() // since we don't have Glitch doing this for us
+"use strict"
+require("dotenv").config() // since we don't have Glitch doing this for us
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const dns = require('dns')
-const { promisify } = require('util')
-const url = require('url')
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const dns = require("dns")
+const { promisify } = require("util")
+const url = require("url")
 
 const app = express()
 
@@ -20,9 +20,9 @@ mongoose.connect(
   { useMongoClient: true }
 )
 const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function () {
-  console.log('Notice: db connected!')
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", function() {
+  console.log("Notice: db connected!")
 })
 
 //
@@ -37,19 +37,19 @@ const CounterSchema = Schema({
   _id: { type: String, required: true },
   seq: { type: Number, default: 0 }
 })
-const Counter = mongoose.model('Counter', CounterSchema)
+const Counter = mongoose.model("Counter", CounterSchema)
 
 const UrlSchema = new Schema({
   original_url: { type: String, unique: true },
   short_url: Number
 })
 
-UrlSchema.pre('save', function (next) {
+UrlSchema.pre("save", function(next) {
   const doc = this
   // Use option upsert to create when non-existent and new to return seq value
   // when created
   Counter.findByIdAndUpdate(
-    { _id: 'urlId' },
+    { _id: "urlId" },
     { $inc: { seq: 1 } },
     { upsert: true, new: true }
   )
@@ -60,13 +60,13 @@ UrlSchema.pre('save', function (next) {
     .catch(next)
 })
 
-const Url = mongoose.model('Url', UrlSchema)
+const Url = mongoose.model("Url", UrlSchema)
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/public', express.static(process.cwd() + '/public'))
+app.use("/public", express.static(process.cwd() + "/public"))
 
 const lookupAsync = promisify(dns.lookup)
 
@@ -76,12 +76,12 @@ const catchErrors = fn => (req, res, next) => fn(req, res, next).catch(next)
 
 // Catch unhandled promise rejections
 // Wes Bos: https://www.youtube.com/watch?v=DwQJ_NPQWWo
-process.on('unhandledRejection', error => {
-  console.log('unhandledRejection', error)
+process.on("unhandledRejection", error => {
+  console.log("unhandledRejection", error)
 })
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html')
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
 })
 
 // Respond with existing short and long URL if long is found
@@ -102,19 +102,16 @@ const validateUrl = async (req, res, next) => {
   const urlParsed = url.parse(req.body.url)
 
   // Check for valid protocol and that it has a hostname
-  if (
-    !['http:', 'https:'].includes(urlParsed.protocol) ||
-    !urlParsed.hostname
-  ) {
-    return res.json({ error: 'invalid URL' })
+  if (!["http:", "https:"].includes(urlParsed.protocol) || !urlParsed.hostname) {
+    return res.json({ error: "invalid URL" })
   }
 
   // Check hostname can be resolved
   try {
     await lookupAsync(urlParsed.hostname)
   } catch (err) {
-    if (err.message.startsWith('getaddrinfo ENOTFOUND')) {
-      return res.json({ error: 'invalid Hostname' })
+    if (err.message.startsWith("getaddrinfo ENOTFOUND")) {
+      return res.json({ error: "invalid Hostname" })
     }
     next(err)
   }
@@ -134,19 +131,16 @@ const addUrl = async (req, res, next) => {
 
 // Redirect from short URL to original
 const redirectFromShortURL = async (req, res, next) => {
-  const data = await Url.findOne(
-    { short_url: req.params.short_url },
-    'original_url'
-  )
+  const data = await Url.findOne({ short_url: req.params.short_url }, "original_url")
   if (!data) {
-    return res.json({ error: 'No short url found for given input' })
+    return res.json({ error: "No short url found for given input" })
   }
   return res.redirect(data.original_url)
 }
 
 // Short URL creation
 app.post(
-  '/api/shorturl/new',
+  "/api/shorturl/new",
   catchErrors(respondWithExisting),
   catchErrors(validateUrl),
   catchErrors(addUrl),
@@ -154,8 +148,8 @@ app.post(
 )
 
 // Redirect from short URL to original
-app.get('/api/shorturl/:short_url', catchErrors(redirectFromShortURL))
+app.get("/api/shorturl/:short_url", catchErrors(redirectFromShortURL))
 
 app.listen(port, () => {
-  console.log('Node.js listening ...')
+  console.log("Node.js listening ...")
 })
